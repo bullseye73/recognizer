69ffe3877f16a17d6c1b8a2ad0ca539fd93beed9
bullseye73 <bullseye73@gmail.com>
2019-08-14 13:16:44 +0900

    Tyler 작업내용 머지 및 BL 인식률 개선

--- src/recognizer_module.cpp
+++ src/recognizer_module.cpp
@@ -1,4 +1,5 @@
-﻿#include <Windows.h>
+﻿//﻿
+#include <Windows.h>
 
 #include <boost/algorithm/string.hpp>
 #include <spdlog/spdlog.h>
@@ -19,6 +20,7 @@
 #include <boost/locale.hpp>
 // TEST
 
+
 std::string
 to_cp949__(const std::wstring& str)
 {
@@ -33,86 +35,48 @@ to_cp949__(const std::wstring& str)
 }
 
 
-
 int
 main()
-{	
-	//auto lower_text = boost::regex_replace(L"", boost::wregex(L"[^/ 0-9a-z\\.-]"), L"");
-	//cv::Mat image = cv::imread("E:/20190329104923-1.jpg");
+{
+	spdlog::stdout_color_mt("console");
 
-	//cv::Mat matrix = cv::getRotationMatrix2D(cv::Point(image.cols / 2, image.rows / 2), 3, 1);
-	//cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), image.size(), 3).boundingRect2f();
-	//matrix.at<double>(0, 2) += bbox.width / 2.0 - image.cols / 2.0;
-	//matrix.at<double>(1, 2) += bbox.height / 2.0 - image.rows / 2.0;	
+	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-	//cv::Mat dst;
-	//cv::warpAffine(image, dst, matrix, bbox.size(), 1, cv::BORDER_REPLICATE);
-	//cv::imwrite("E:/dst_03.jpg", dst);
+	selvy::ocr::recognizer_factory::initialize();
 
-    spdlog::stdout_color_mt("console");
+	const auto image_directory = L"D:\\workspace\\OCR\\IBK\\images";
 
-    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+	const auto profile = L"trade";
+	std::wstring dataset_directory = L"D:\\workspace\\OCR\\IBK\\images";// fmt::format(L"{}\\{}\\datasets", image_directory, profile);
 
-    selvy::ocr::recognizer_factory::initialize();
+	auto recognizer = selvy::ocr::recognizer_factory::create(profile);
 
-    const auto image_directory = L"C:\\workspace\\vs2015\\images";
-    const auto profile = L"trade";
-    // std::wstring dataset_directory =L"D:\\ksjo\\images\\sample_report" ;// fmt::format(L"{}\\{}\\datasets", image_directory, profile);
-	// std::wstring dataset_directory = L"D:\\test";
-	std::wstring dataset_directory = L"C:\\workspace\\vs2015\\images";
-	
-    auto recognizer = selvy::ocr::recognizer_factory::create(profile);
-
-//    std::vector<std::wstring> categories = {
-//        L"document",
-//    };
 	std::vector<std::wstring> categories = {
-		L"AIR WAYBILL",              //0
-		L"BILL OF EXCHANGE",         //1
-		L"BILL OF EXCHANGE-KOR",     //2
-		L"BILL OF LADING",           //3
-		L"CARGO RECEIPT",            //4
-		L"CERTIFICATE",              //5
-		L"CERTIFICATE OF ORIGIN",    //6
-		L"EXPORT PERMIT",            //7
-		L"INSURANCE POLICY",         //8
-		L"LETTER OF CREDIT",         //9
-		L"PACKING LIST",             //10
-		L"REMITTANCE LETTER",        //11
-		L"SHIPMENT ADVICE",          //12
-		L"WAYBILL",                  //13
-		L"COMMERCIAL INVOICE",       //14
-		L"LETTER OF GUARANTEE",      //15
-		L"FIRM OFFER",               //16
-		L"PROFORMA INVOICE",               //17
+		L"INCOME",				//0
+		L"PENSION",				//1
+		L"SMALL",				//2
+		L"MIDDLE",				//3
+		L"COMMERCIAL INVOICE",	//4
+								//
+								L"BILL OF LADING",		//5
+								L"PROFOMA INVOICE",		//6
+								L"LETTER OF CREDIT",	//7
+
 	};
 
-//    std::unordered_map<std::wstring, std::vector<std::wstring>> key_values = {
-//		{ categories[0], { L"COURT NAME", L"EVENT", L"CREDITOR", L"DEBTOR", L"DEBTOR_OTHER", L"PRICE", L"JUDGE" } },
-//    };
-
 	std::unordered_map<std::wstring, std::vector<std::wstring>> key_values = {
-		{ categories[0],{ L"PORT OF LOADING", L"PORT OF DISCHARGE", L"CONSIGNEE", L"SHIPPER", L"NOTIFY", L"VESSEL NAME", L"AGENT" } },
-		{ categories[1],{ L"NEGOTIATION BANK" } },
-		{ categories[2],{ L"" } },
-		{ categories[3],{ L"CONSIGNEE", L"CARRIER", L"NOTIFY", L"PORT OF DISCHARGE", L"PORT OF LOADING", L"SHIPMENT DATE", L"VESSEL NAME" } },
-		//{ categories[3],{ L"PORT OF LOADING", L"PLACE OF RECEIPT", L"PORT OF DISCHARGE", L"PLACE OF DELIVERY", L"CONSIGNEE", L"SHIPPER", L"NOTIFY", L"VESSEL NAME", L"CARRIER", L"PLACE OF ISSUE", L"ORIGIN", L"SHIPPING LINE" } },
-		{ categories[4],{ L"PORT OF LOADING", L"PLACE OF RECEIPT", L"PORT OF DISCHARGE", L"PLACE OF DELIVERY", L"CONSIGNEE", L"SHIPPER", L"NOTIFY", L"VESSEL NAME", L"PLACE OF ISSUE" } },
-		{ categories[5],{ L"ORIGIN", L"GOODS DESCRIPTION", L"PORT OF LOADING", L"PLACE OF DELIVERY", L"PORT OF DISCHARGE", L"VESSEL NAME", } },
-		{ categories[6],{ L"ORIGIN", L"IMPORTER" } },
-		{ categories[7],{ L"GOODS DESCRIPTION" } },
-		{ categories[8],{ L"INSURANCE COMPANY", L"INSURANCE SETTLING AGENT", L"INSURANCE SURVEY AGENT", L"PORT OF LOADING", L"PORT OF DISCHARGE", L"VESSEL NAME", L"ORIGIN", L"GOODS DESCRIPTION" } },
-		//{ categories[9],{ L"ISSUING BANK", L"COLLECTING BANK", L"APPLICANT", L"BENEFICIARY", L"PORT OF LOADING", L"PORT OF DISCHARGE" } },
-		{ categories[9],{ L"SWIFT MT SENDER", L"AMOUNT", L"CREDIT NUMBER" } },
-		{ categories[10],{ L"SELLER", L"BUYER", L"ORIGIN", L"CONSIGNEE", L"NOTIFY", L"PORT OF LOADING", L"EXPORTER", L"APPLICANT", L"MANUFACTURER", L"PORT OF DISCHARGE", L"PLACE OF DELIVERTY", L"VESSEL NAME" } },
-		{ categories[11],{ L"ISSUING BANK", L"DRAWER" } },
-		{ categories[12],{ L"APPLICANT", L"BENEFICIARY", L"PORT OF LOADING", L"PORT OF DISCHARGE", L"VESSEL NAME", L"SHIPPING LINE", L"INSURANCE COMPANY" } },
-		{ categories[13],{ L"", } },
-		//{ categories[14],{ L"SELLER", L"BUYER", L"ORIGIN", L"CONSIGNEE", L"NOTIFY", L"PORT OF LOADING", L"EXPORTER", L"APPLICANT", L"MANUFACTURER", L"PORT OF DISCHARGE", L"PLACE OF DELIVERY", L"VESSEL NAME" } },
-		{ categories[14],{ L"SELLER", L"AMOUNT", L"L/C" } },
-		{ categories[15],{ L"CARRIER", L"SHOPPER", L"PORT OF LOADING", L"PORT OF DISCHARGE", L"VESSEL NAME", L"CONSIGNEE", L"GOODS DESCRIPTION", L"NOTIFY" } },
-		{ categories[16],{ L"SELLER", L"BUYER", L"ORIGIN", L"APPLICANT", L"COLLECTING BANK" } },
-		{ categories[17],{ L"BANK NAME", L"SWIFT CODE"} },
+		{ categories[0],{ L"사업자번호", L"법인명", L"대표자", L"퇴직사유", L"퇴직급여", L"입사일", L"기산일",L"퇴사일",L"지급일",L"계좌번호",L"계좌입금금액" } },
+		{ categories[1],{ L"사업자번호", L"근무처명", L"입사일", L"기산일",L"퇴사일",L"지급일",L"계좌번호",L"입금일", L"계좌입금금액" } },
+		//{ categories[2],{ L"금융기관용", L"유효기간", L"발급번호", L"업체명",L"대표자",L"사업자등록번호",L"자금명",L"최대한도액", L"담보구분" } },
+		{ categories[2],{ L"유효기간", L"발급번호", L"업체명",L"대표자",L"사업자등록번호",L"자금명" } },
+		//{ categories[3],{ L"제목", L"업체명", L"대표자", L"대출취급기한",L"구분",L"금액",L"담보종류",L"취급기관", L"대출기간", L"금리" } },
+		{ categories[3],{ L"제목", L"업체명", L"대표자",L"구분",L"금액",L"담보종류",L"취급기관", L"대출기간", L"금리", L"대출취급기한" } },
+		{ categories[4],{ L"SELLER", L"L/C NO", L"AMOUNT", L"CURRENCY SIGN" } },
+		//claude
+		{ categories[5],{ L"CONSIGNEE", L"CARRIER/MASTER/OWNER", L"NOTIFY", L"PORT OF DISCHARGE", L"PORT OF LOADING", L"ON BOARD DATE", L"VESSEL NAME" } }, //BL
+																																							//{ categories[6],{ L"SELLER", L"AMOUNT", L"L/C" } },
+		{ categories[7],{ L"SWIFT MT SENDER", L"AMOUNT", L"CREDIT NUMBER" } },//취소불능화환신용장
+																			  //{ categories[14],{ L"SELLER", L"BUYER", L"ORIGIN", L"CONSIGNEE", L"NOTIFY", L"PORT OF LOADING", L"EXPORTER", L"APPLICANT", L"MANUFACTURER", L"PORT OF DISCHARGE", L"PLACE OF DELIVERY", L"VESSEL NAME", L"L/C" } },
 	};
 
 
@@ -130,11 +94,13 @@ main()
 
 	std::sort(std::begin(files), std::end(files), compareNat);
 
+	std::wstring res;
 	for (const auto& file : files) {
-		auto category = L"BILL OF LADING";// categories[14];
-		//auto category = L"LETTER OF CREDIT"; //
-		//auto category = L"COMMERCIAL INVOICE";
-		//auto category = L"PROFORMA INVOICE"; //
+		//auto category = categories[5];
+		auto category = L"BILL OF LADING";		//5
+												//			L"PROFOMA INVOICE",		//6
+												//			L"LETTER OF CREDIT",	//7
+
 		const auto fields = recognizer->recognize(file, category);
 
 		auto output_path = fmt::format(L"{}_result.txt", boost::filesystem::path(file).native());
@@ -146,24 +112,34 @@ main()
 
 		auto& keys = key_values.at(category);
 		int count = 1;
-		result_text += fmt::format(L"\"file name\",\"{}\"\n", fields.at(L"FILE NAME").front());
+		//result_text += fmt::format(L"\"file name\",\"{}\"\n", fields.at(L"FILE NAME").front());
+		res += fmt::format(L"\"set name\",\"{}\"\n", boost::filesystem::path(file).filename().native());
+		res += fmt::format(L"\"file name\",\"{}\"\n", boost::filesystem::path(file).filename().native());
+		res += fmt::format(L"\"category\",\"{}\"\n", category);
 		result_text += fmt::format(L"\"category\",\"{}\"\n", category);
 		count++;
 		for (auto& key : keys) {
 			auto& name = key;
 			result_text += L"\"" + name + L"\",";
+			res += L"\"" + name + L"\",";
 			if (fields.find(name) == fields.end()) {
 				result_text += L"\n";
+				res += L"\n";
 				continue;
 			}
 			auto& r = fields.at(name);
 
 			for (auto& f : r) {
 				result_text += L"\"" + f + L"\",";
+				res += L"\"" + f + L"\",";
 			}
 			result_text += L"\n";
+			res += L"\n";
 		}
 		result_text += L"\n";
+		res += L"\n";
+		res += L"\n";
+
 		std::wofstream txt_file;
 		txt_file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
 		txt_file.open(output_path, std::wofstream::out | std::wofstream::ate);
@@ -171,6 +147,15 @@ main()
 		txt_file.close();
 
 	}
+	auto cur = time(NULL);
+	auto time = localtime(&cur);
+	auto output_path = fmt::format(L"\\result_{:02d}{:02d}.txt", time->tm_mon + 1, time->tm_mday);
+
+	std::wofstream res_all;
+	res_all.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
+	res_all.open(dataset_directory + output_path, std::wofstream::out | std::wofstream::ate);
+	res_all << res << std::endl;
+	res_all.close();
 
 	spdlog::drop_all();
 
@@ -181,7 +166,7 @@ main()
 
 int
 main2()
-{	
+{
 	//auto lower_text = boost::regex_replace(L"", boost::wregex(L"[^/ 0-9a-z\\.-]"), L"");
 	//cv::Mat image = cv::imread("E:/20190329104923-1.jpg");
 
@@ -194,29 +179,30 @@ main2()
 	//cv::warpAffine(image, dst, matrix, bbox.size(), 1, cv::BORDER_REPLICATE);
 	//cv::imwrite("E:/dst_03.jpg", dst);
 
-    spdlog::stdout_color_mt("console");
+	spdlog::stdout_color_mt("console");
 
-    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-    selvy::ocr::recognizer_factory::initialize();
+	selvy::ocr::recognizer_factory::initialize();
 
-    const auto image_directory = L"E:\\2019_PROJECT\\99.ETC\\추심\\문서이미지";
-    const auto profile = L"idcard";
-    // std::wstring dataset_directory =L"D:\\ksjo\\images\\sample_report" ;// fmt::format(L"{}\\{}\\datasets", image_directory, profile);
+	const auto image_directory = L"D:\\work\\190710(kb_entrust)\\sample_kb";
+	const auto profile = L"entrust";
+
+	// std::wstring dataset_directory =L"D:\\ksjo\\images\\sample_report" ;// fmt::format(L"{}\\{}\\datasets", image_directory, profile);
 	// std::wstring dataset_directory = L"D:\\test";
-	 std::wstring dataset_directory = L"E:\\awork\\sample3";
+	std::wstring dataset_directory = L"D:\\work\\190710(kb_entrust)\\sample_kb";
 
-    auto recognizer = selvy::ocr::recognizer_factory::create(profile);
+	auto recognizer = selvy::ocr::recognizer_factory::create(profile);
 
-    std::vector<std::wstring> categories = {
-        L"document",
-    };
+	std::vector<std::wstring> categories = {
+		L"document",
+	};
 
-    std::unordered_map<std::wstring, std::vector<std::wstring>> key_values = {
-		{ categories[0], { L"COURT NAME", L"EVENT", L"CREDITOR", L"DEBTOR", L"DEBTOR_OTHER", L"PRICE", L"JUDGE" } },
-    };
+	std::unordered_map<std::wstring, std::vector<std::wstring>> key_values = {
+		{ categories[0],{ L"발행기관", L"계좌번호", L"상품명", L"매수일자", L"매수금액", L"투자기간", L"이율(연)",L"상환이자",L"상환금액" } },
+	};
 
-    std::vector<std::wstring> files;
+	std::vector<std::wstring> files;
 
 	for (auto& entry : boost::filesystem::recursive_directory_iterator(dataset_directory)) {
 		const auto file = entry.path();
@@ -229,25 +215,26 @@ main2()
 			files.emplace_back(boost::filesystem::absolute(file).native());
 		}
 	}
-	
+
 	std::sort(std::begin(files), std::end(files), compareNat);
 
-    for (const auto& file : files) {
+	for (const auto& file : files) {
 		cv::TickMeter processing_ticks;
 		processing_ticks.start();
 
 		const auto fields = recognizer->recognize(file, categories[0]);
 
 		auto output_path = fmt::format(L"{}{}_result.csv", OUTPUT_FOLDER, boost::filesystem::path(file).filename().native());
-        std::wstring result_text;
+		std::wstring result_text;
 
 		if (fields.empty()) {
-            continue;
-        }
+			continue;
+		}
 
-		if (profile == L"court") {
-			auto keys = std::vector<std::wstring>{ L"COURT NAME", L"EVENT", L"CREDITOR", L"DEBTOR", L"DEBTOR_OTHER", L"PRICE", L"JUDGE" };
-			auto mames = std::vector<std::wstring>{ L"법원명", L"사건", L"채권자", L"채무자", L"제3채무자", L"채권금액", L"판사" };
+		if (std::wcscmp(profile, L"entrust") == 0) {
+			//auto keys = std::vector<std::wstring>{ L"ISSUING ENTITY", L"ACCOUNT NUMBER", L"PRODUCT NAME", L"PURCHASE DATE", L"PURCHASE AMOUNT", L"INVESTMENT PERIOD", L"INTEREST RATE", L"상환이자", L"상환금액" };
+			auto keys = std::vector<std::wstring>{ L"발행기관", L"계좌번호", L"상품명", L"매수일자", L"매수금액", L"투자기간", L"이율(연)",L"상환이자",L"상환금액" };
+			auto mames = std::vector<std::wstring>{ L"발행기관", L"계좌번호", L"상품명", L"매수일자", L"매수금액", L"투자기간", L"이율(연)",L"상환이자",L"상환금액" };
 			//result_text += fmt::format(L"\"file name\",\"{}\"\n", fields.at(L"FILE NAME").front());
 			//      result_text += fmt::format(L"\"category\",\"{}\"\n", category);
 			int i = 0;
@@ -265,25 +252,25 @@ main2()
 				else {
 					for (int i = 0; i < r.size(); ++i) {
 						auto& f = r[i];
-						result_text += fmt::format(L"\"{} {:03d}\",\"{}\"\n", name, i, f);
+						result_text += fmt::format(L"\"{} \",\"{}\"\n", name, f);
 					}
 				}
-			}			
+			}
 		}
 
-        result_text += L"\n";
-        std::wofstream txt_file;
-        txt_file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
-        txt_file.open(output_path, std::wofstream::out | std::wofstream::ate);
-        txt_file << result_text << std::endl;
-        txt_file.close();
+		result_text += L"\n";
+		std::wofstream txt_file;
+		txt_file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
+		txt_file.open(output_path, std::wofstream::out | std::wofstream::ate);
+		txt_file << result_text << std::endl;
+		txt_file.close();
 
 		processing_ticks.stop();
 		spdlog::get("recognizer")->info("process total : {} ({:.2f}mSec)", to_cp949__(boost::filesystem::path(file).filename().native()),
 			processing_ticks.getTimeMilli());
-    }
+	}
 
-    spdlog::drop_all();
+	spdlog::drop_all();
 
-    return 0;
+	return 0;
 }
